a09991ccdc164da682e061b5f13800c076fd0575
venimu <ven1mu@yandex.ru>
2023-10-09 18:43:50 +0300

    fix(Game.py): fixed bugs about speed of bullet, left click and FPS

---
+++ example.py
@@ -0,0 +1,20 @@
+ resources: [ 
+        {name: "bg", type:"image", src: "bg.png"},
+        {name: "clumsy", type:"image", src: "clumsy.png"},
+        {name: "pipe", type:"image", src: "pipe.png"},
+        {name: "logo", type:"image", src: "logo.png"},
+        {name: "ground", type:"image", src: "ground.png"},
+        {name: "gameover", type:"image", src: "gameover.png"},
+        {name: "gameoverbg", type:"image", src: "gameoverbg.png"},
+        {name: "hit", type:"image", src: "hit.png"},
+        {name: "getready", type:"image", src: "getready.png"},
+        {name: "new", type:"image", src: "new.png"},
+        {name: "share", type:"image", src: "share.png"},
+        {name: "tweet", type:"image", src: "tweet.png"},
+        // sounds
+        {name: "theme", type: "audio", src: "data/bgm/"},
+        {name: "hit", type: "audio", src: "data/sfx/"},
+        {name: "lose", type: "audio", src: "data/sfx/"},
+        {name: "wing", type: "audio", src: "data/sfx/"},
+
+    ]
